89e997abb4d8c44ce1eab8bb81f13c4b39bcc4f1
mab98 <mhmdabubakar98@gmail.com>
2025-07-16 16:01:54 +0500

    init: Clean up app module and routing module structure
    - Fromatted files.
    - Removed placeholder content from app.html.
    - Deleted unused app.spec.ts file.
    - Added global box-sizing and reset styles in styles.css.
    - Update angular.json to skip tests and cli analytics.

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,20 +1,13 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { AppRoutingModule } from './app-routing-module';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners()
-  ],
-  bootstrap: [App]
+  declarations: [App],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
